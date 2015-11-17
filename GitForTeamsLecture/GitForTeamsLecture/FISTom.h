95550f841225fee66507a75c14edf73de2e49932
Tim Clem <tim.clem@gmail.com>
2015-11-17 10:37:04 -0500

    Add Tom model

---
+++ GitForTeamsLecture/GitForTeamsLecture/FISTom.h
@@ -0,0 +1,15 @@
+//
+//  FISTom.h
+//  GitForTeamsLecture
+//
+//  Created by Timothy Clem on 11/17/15.
+//  Copyright © 2015 Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISTom : NSObject
+
+@property (nonatomic, assign) BOOL isTheBest;
+
+@end
---
+++ GitForTeamsLecture/GitForTeamsLecture/FISTom.m
@@ -0,0 +1,23 @@
+//
+//  FISTom.m
+//  GitForTeamsLecture
+//
+//  Created by Timothy Clem on 11/17/15.
+//  Copyright © 2015 Flatiron School. All rights reserved.
+//
+
+#import "FISTom.h"
+
+@implementation FISTom
+
+-(instancetype)init
+{
+    self = [super init];
+    if(self) {
+        _isTheBest = YES;
+    }
+
+    return self;
+}
+
+@end
