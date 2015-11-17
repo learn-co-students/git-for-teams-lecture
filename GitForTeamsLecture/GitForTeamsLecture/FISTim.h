63752e859d5fce587375ebbceca79601ae93dbdd
TOMalley104 <tom@flatironschool.com>
2015-11-17 10:15:37 -0500

    added model class

---
+++ GitForTeamsLecture/GitForTeamsLecture/FISTim.h
@@ -0,0 +1,18 @@
+//
+//  FISTim.h
+//  GitForTeamsLecture
+//
+//  Created by Tom OMalley on 11/17/15.
+//  Copyright © 2015 Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISTim : NSObject
+
+@property (nonatomic, strong) NSString *name; 
+@property (nonatomic) BOOL isWearingABeanie;
+@property (nonatomic, strong) NSString *mood;
+@property (nonatomic, strong) NSString *currentFavoriteEmoji;
+
+@end
